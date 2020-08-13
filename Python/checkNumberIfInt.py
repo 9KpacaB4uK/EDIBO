4d23b9565d4fd02f31ae62839ed4d163d056d31f
user <sanja81000@gmail.com>
2020-08-13 16:46:15 +0300

    Added second variant

--- Python/checkNumberIfInt.py
+++ Python/checkNumberIfInt.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3.6
-
-
-a = (input("Enter number: "))
-if a.isdigit(): print(a)
-else: print("error: It is not int number")
-
+print("#########","Check 2^n","###########")
+a = 0
+while (a != "1" or a != "2"):
+	a = (input("Choose variant(1 or 2): "))
+	if (a == "1"):
+		#1 variant
+		print("1.variant")
+		a = (input("Enter number: "))
+		if a.isdigit():
+			print("2 ^",a," = ",2**int(a))
+			break;
+		else:
+			print("error: It is not int number")
+			break;
+	elif (a == "2"):
+		#2 variant
+		print("2.variant")
+		try:
+			a = int(input("Enter number: "))
+			print("2 ^",a," = ",2**int(a))
+			break
+		except:
+			print("error: It is not int number")
+			break
+	else:
+		print("Error: input only '1' or '2'")
