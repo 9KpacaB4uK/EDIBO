0a888662de052f847b6017b65ae680fc6ad8db4b
Alexander <sanja81000@gmail.com>
2020-08-20 19:41:38 +0300

    fix github

--- dotNet/decToBin/Program.cs
+++ dotNet/decToBin/Program.cs
@@ -13,17 +13,21 @@ namespace decToBin
                 Console.Write("Enter a Number : ");
                 inputNumber = Convert.ToInt32(Console.ReadLine());
                 num = Convert.ToInt32(inputNumber);
-            result = "";
-            while (num > 1)
-            {
-                remainder = num % 2;
-                result = Convert.ToString(remainder) + result;
-                Console.WriteLine(num+"%"+2+"="+remainder+"\t|"+result);
-                num /= 2;
-            }
-            result = Convert.ToString(num) + result;
-            Console.WriteLine(num+"%"+2+"="+remainder+"\t|"+result);
-            Console.WriteLine("Answer"+"\t|"+result);
+                if(num>=0){
+                    result = "";
+                    while (num > 1)
+                    {
+                        remainder = num % 2;
+                        result = Convert.ToString(remainder) + result;
+                        Console.WriteLine(num+"%"+2+"="+remainder+"\t|"+result);
+                        num /= 2;
+                    }
+                    result = Convert.ToString(num) + result;
+                    Console.WriteLine(num+"%"+2+"="+remainder+"\t|"+result);
+                    Console.WriteLine("Answer"+"\t|"+result);
+                }else{
+                    Console.Write("Error 1: number is < 0");
+                }
             }catch(Exception e){
                 Console.Write("Error 0: "+e.Message);
             }
--- dotNet/fibonacciNumbers/Program.cs
+++ dotNet/fibonacciNumbers/Program.cs
@@ -11,20 +11,24 @@ namespace fibonacciNumbers
             try{
                 Console.Write("Enter number of rows: ");
                 rows = Convert.ToInt32(Console.ReadLine());
+                if(rows >= 0){
+                    for(int i=0;i<rows;i++){
+                        for(int j=0;j<=i;j++){
+                            if(j == 0 || i == 0){
+                                koef = 1;
+                                Console.WriteLine();//new line
+                            }else{
+                                koef=koef*(i-j+1)/j;
+                                Console.Write("_");//space
+                            }
+                            Console.Write(koef);
+                        }
+                    }
+                }else{
+                   Console.WriteLine("Error 1: number < 0"); 
+                }
             }catch(Exception e){
                 Console.WriteLine("Error 0: "+e.Message);
-            }
-            for(int i=0;i<rows;i++){
-                for(int j=0;j<=i;j++){
-                    if(j == 0 || i == 0){
-                        koef = 1;
-                        Console.WriteLine();
-                    }else{
-                        koef=koef*(i-j+1)/j;
-                        Console.Write("_");
-                    }
-                    Console.Write(koef);
-                }
             }
 
         }
